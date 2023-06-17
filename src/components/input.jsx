e48ad789acf808398bcf4b359585b90f765be200
Pawel Jan Wilk <pwilk0108@gmail.com>
2023-06-17 20:18:04 +0200

    new way of inserting the letters

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -2,35 +2,58 @@ import { useState } from 'react';
 import config from '../config';
 
 function Input(props) {
-    const [letters, setLetters] = useState('');
+    const [letter, setLetter]   = useState('');
+    const [letters, setLetters] = useState(['d', 'u', 'p', 'a']);
 
-    const onKeypress = (e) => {
+   const onKeypress = (e) => {
         const keyPressed = e.code;
         const letterUsed = e.key;
         const {debug} = config;
 
         if (keyPressed === 'Backspace') {
-            let actualStr = letters;
-            actualStr = actualStr.slice(0, -1);
-
-            setLetters(actualStr);
-            props.letterChange(actualStr);
+            setLetters( letters.slice(0, -1) )
+            // props.letterChange(letters);
         } else if (['Enter', ' ', 'Tab', 'Space'].indexOf(keyPressed) > -1) {
-            if (debug) console.log(`Input.jsx: Special key: ${keyPressed} is used in script!`); 
+            debug && console.log(`Input.jsx: Special key: ${keyPressed} is used in script!`); 
         } else if (letterUsed.length > 1) {
-            if (debug) console.log(`Input.jsx: Special key: ${keyPressed} is not used in script!`); 
+            debug && console.log(`Input.jsx: Special key: ${keyPressed} is not used in script!`); 
         } else {
-            if (debug) console.log(`Input.jsx: Key appended: ${keyPressed}`); 
+            debug && console.log(`Input.jsx: Key appended: ${keyPressed}`); 
             
-            setLetters(letters + letterUsed);
-            props.letterChange(letters + letterUsed);
+            setLetters([...letters, letterUsed]);
+            setLetter('');
+            // props.letterChange(letters);
+        }
+    }
+
+    const InputField = (props) => {
+        {
+            return (letters.length >= props.letterCount) && (
+                <input 
+                    type="text"
+                    style={{'width':'25px'}}
+                    disabled="disabled"
+                    value={props.value}
+                />
+            )
         }
     }
 
     return (
         <>
-            {letters}
-            <input type="text" name="wordInput" onKeyDown={(e) => onKeypress(e)} value={letters} />
+            {
+                letters.map((letter, key) => (
+                    <InputField key={key} letterCount={key} value={letter}/>
+                ))
+
+            }
+
+            <input 
+                type="text"
+                style={{'width':'25px'}}
+                onKeyDown={onKeypress}
+                value={letter}
+            />
         </>
     )
 }
