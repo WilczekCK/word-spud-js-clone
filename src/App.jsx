ab99d60f46088f0ff7be5489faf3b43c27f86452
Pawel Jan Wilk <pwilk0108@gmail.com>
2023-06-25 21:06:09 +0200

    display words in loop of array

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import Input from './components/input';
 import useEndOfWord from './hooks/useEndOfWord';
 
 function App() {
-  const [word, setWord] = useState('');
+  const [words, setWords] = useState([]);
 
   const changeWord = (v) => {
-    setWord(v);
+    setWords([...words, v]);
     console.log( useEndOfWord(v) );
   }
 
   return (
     <>
       <h1>Word spud</h1>
-      {word}
-      <Input appendedWord={(v) => changeWord(v)}/>
+      <ul>
+        {words.map(word => (
+          <li>{word}</li>
+        ))}
+      </ul>
+
+      <Input 
+        appendedWord={(v) => changeWord(v)}
+      />
     </>
   )
 }
